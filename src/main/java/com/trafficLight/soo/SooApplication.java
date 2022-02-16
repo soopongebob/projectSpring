fc6e3f8d4f27fde4635cd0b650410d94457740ed
soopongebob <92519479+soopongebob@users.noreply.github.com>
2022-02-16 19:35:55 +0900

    setting static resource path

--- src/main/java/com/trafficLight/soo/SooApplication.java
+++ src/main/java/com/trafficLight/soo/SooApplication.java
@@ -2,10 +2,22 @@ package com.trafficLight.soo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class SooApplication {
+@Configuration
+@EnableWebMvc
+public class SooApplication implements WebMvcConfigurer {
 
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry){
+		registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/static/assets/");
+		registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
+		registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SooApplication.class, args);
 	}
