ac6431ffce24e6f81675b15986bf9fc463ad8ba2
soopongebob <92519479+soopongebob@users.noreply.github.com>
2022-02-18 16:38:28 +0900

    Controller - 로그인, 회원가입 controller 수정

--- src/main/java/com/trafficLight/soo/controller/HomeController.java
+++ src/main/java/com/trafficLight/soo/controller/HomeController.java
@@ -1,7 +1,9 @@
 package com.trafficLight.soo.controller;
 
+import com.trafficLight.soo.entity.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
 public class HomeController {
--- src/main/java/com/trafficLight/soo/controller/UserController.java
+++ src/main/java/com/trafficLight/soo/controller/UserController.java
@@ -10,6 +10,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.util.UUID;
 
@@ -18,6 +20,7 @@ import java.util.UUID;
 public class UserController {
 
     private final UserService userService;
+    private HttpSession httpSession;
 
     //마이페이지
     @GetMapping("/users/myPage")
@@ -32,6 +35,26 @@ public class UserController {
         return "users/signIn";
     }
 
+    //로그인 확인
+    @PostMapping("/users/signIn")
+    public String signIn(@Valid SignInForm signInForm, BindingResult bindingResult,
+                         HttpServletRequest request){
+
+        //오류확인
+        if(bindingResult.hasErrors()){
+            return "users/signIn";
+        }
+
+        String userId = userService.checkIdAndPwd(signInForm);
+        if(userId != null){
+            httpSession = request.getSession();
+            httpSession.setAttribute("loginUser", userId);
+            return "index";
+        }else{
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호 오류입니다.");
+            return "users/signInFail";
+        }
+    }
     //회원가입 페이지
     @GetMapping("/users/signUp")
     public String signUp(Model model) {
@@ -59,7 +82,28 @@ public class UserController {
                 .auth(auth)
                 .build();
 
-        userService.signUp(user);
-        return "index";
+        //return null일 경우, 가입 완료
+        String check = userService.signUp(user);
+        if(check == null){      //성공
+            return "index";
+        }else{
+            return "users/signUpFail";
+        }
+    }
+
+    @GetMapping("/users/signUpFail")
+    public String signUpFail(){
+        return "users/signUpFail";
+    }
+
+    @PostMapping("/users/logout")
+    public String logout(HttpServletRequest request){
+        httpSession = request.getSession(false);
+
+        if(httpSession != null){
+            httpSession.invalidate();
+        }
+
+        return "redirect:/";
     }
 }
