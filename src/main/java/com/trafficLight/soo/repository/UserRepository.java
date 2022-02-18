92964afeba7e1e2dc389e7b5a324b8455f9cb9f2
soopongebob <92519479+soopongebob@users.noreply.github.com>
2022-02-18 16:39:01 +0900

    Model - 로그인 회원가입 기능추가

--- src/main/java/com/trafficLight/soo/repository/UserRepository.java
+++ src/main/java/com/trafficLight/soo/repository/UserRepository.java
@@ -3,8 +3,11 @@ package com.trafficLight.soo.repository;
 import com.trafficLight.soo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
     User save(User user);
+    Optional<User> findByUserId(String userId);
+    Optional<User> findByUserIdAndPassword(String userId, String password);
 }
--- src/main/java/com/trafficLight/soo/service/UserService.java
+++ src/main/java/com/trafficLight/soo/service/UserService.java
@@ -1,9 +1,12 @@
 package com.trafficLight.soo.service;
 
+import com.trafficLight.soo.controller.SignInForm;
 import com.trafficLight.soo.entity.User;
 import com.trafficLight.soo.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -11,8 +14,32 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    public void signUp(User user){
-        userRepository.save(user);
+    public String signUp(User user){
+        String result = checkIdDuplicate(user.getUserId());
+        if(result == null) {        //결과가 null이면 성공
+            userRepository.save(user);
+        }
+        return result;
+    }
+
+    //중복확인
+    public String checkIdDuplicate(String userId){
+        Optional<User> user = userRepository.findByUserId(userId);
+        if(user.isEmpty()){     //검색결과가 empty이면 중복되지 않음(성공)
+            return null;
+        }else{
+            return userId;
+        }
+    }
+
+    //로그인 확인
+    public String checkIdAndPwd(SignInForm signInForm){
+        Optional<User> user = userRepository.findByUserIdAndPassword(signInForm.getUserId(), signInForm.getPassword());
+        if(user.isEmpty()){ //결과가 없으면, 로그인 실패
+            return null;
+        }else{
+            return signInForm.getUserId();
+        }
     }
 
 }
