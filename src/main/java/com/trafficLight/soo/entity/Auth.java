507fb9a25af45038d90c389188b7e1c8193d4d0f
soopongebob <92519479+soopongebob@users.noreply.github.com>
2022-02-20 17:22:13 +0900

    Auth 삭제, Role Enum 타입으로 변경

--- src/main/java/com/trafficLight/soo/entity/Auth.java
+++
@@ -1,5 +0,0 @@
-package com.trafficLight.soo.entity;
-
-public enum Auth {
-    MANAGER, USER
-}
---
+++ src/main/java/com/trafficLight/soo/entity/Role.java
@@ -0,0 +1,13 @@
+package com.trafficLight.soo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum Role {
+    ADMIN("ROLE_ADMIN"),
+    USER("ROLE_USER");
+
+    private String value;
+}
