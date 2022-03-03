1ab4035f0a37149787e7a8c5cfe02188d3758e02
soopongebob <92519479+soopongebob@users.noreply.github.com>
2022-03-03 20:31:03 +0900

    검색을 위한 Dto

---
+++ src/main/java/com/trafficLight/soo/controller/SearchCondition.java
@@ -0,0 +1,12 @@
+package com.trafficLight.soo.controller;
+
+import lombok.Data;
+
+@Data
+public class SearchCondition {
+
+    private String subject;
+    private String content;
+    private String username;
+
+}
