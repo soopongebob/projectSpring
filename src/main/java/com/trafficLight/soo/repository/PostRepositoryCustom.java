2da5714aaa2e9fe7061cbeba007053cef5a04ac9
soopongebob <92519479+soopongebob@users.noreply.github.com>
2022-03-03 20:31:58 +0900

    검색기능 쿼리추가

--- src/main/java/com/trafficLight/soo/repository/PostRepositoryCustom.java
+++ src/main/java/com/trafficLight/soo/repository/PostRepositoryCustom.java
@@ -1,9 +1,12 @@
 package com.trafficLight.soo.repository;
 
 import com.trafficLight.soo.controller.PostListForm;
+import com.trafficLight.soo.controller.SearchCondition;
+import com.trafficLight.soo.entity.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface PostRepositoryCustom {
     Page<PostListForm> pagination(Pageable pageable);
+    Page<PostListForm> searchPost(SearchCondition searchCondition, Pageable pageable);
 }
--- src/main/java/com/trafficLight/soo/repository/PostRepositoryImpl.java
+++ src/main/java/com/trafficLight/soo/repository/PostRepositoryImpl.java
@@ -1,15 +1,20 @@
 package com.trafficLight.soo.repository;
 
 import com.querydsl.core.QueryResults;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.trafficLight.soo.controller.PostListForm;
 import com.trafficLight.soo.controller.QPostListForm;
+import com.trafficLight.soo.controller.SearchCondition;
 import com.trafficLight.soo.entity.Post;
 import com.trafficLight.soo.entity.QPost;
 import com.trafficLight.soo.entity.QUser;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 
@@ -43,6 +48,44 @@ public class PostRepositoryImpl implements PostRepositoryCustom{
         List<PostListForm> content = results.getResults();
         long total = results.getTotal();
         return new PageImpl(content, pageable, total);
+    }
+
+    @Override
+    public Page<PostListForm> searchPost(SearchCondition searchCondition, Pageable pageable) {
+        QueryResults<PostListForm> results = queryFactory
+                .select(new QPostListForm(
+                        post.postIdx,
+                        post.subject,
+                        post.user.username,
+                        post.viewCount,
+                        post.createdDate
+                ))
+                .from(post)
+                .where(
+                        subjectEq(searchCondition.getSubject()),
+                        contentEq(searchCondition.getContent()),
+                        usernameEq(searchCondition.getUsername())
+                )
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetchResults();
+
+        List<PostListForm> content = results.getResults();
+        long total = results.getTotal();
+
+        return new PageImpl<>(content, pageable, total);
+    }
+
+    private BooleanExpression usernameEq(String username) {
+        return StringUtils.hasText(username) ? post.user.username.eq(username) : null;
+    }
+
+    private BooleanExpression contentEq(String content) {
+        return StringUtils.hasText(content) ? post.content.eq(content) : null;
+    }
+
+    private BooleanExpression subjectEq(String subject) {
+        return StringUtils.hasText(subject) ? post.subject.eq(subject) : null;
     }
 
 }
